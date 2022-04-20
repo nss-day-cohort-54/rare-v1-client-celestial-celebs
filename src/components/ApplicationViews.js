6d0d0b209737fc8f5ba6c31e8c4f60edfd582591
Airris Williams <airriswilliams83@gmail.com>
2022-04-20 15:25:12 -0500

    PostList

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,8 +1,15 @@
-import React from "react"
-import { Route } from "react-router-dom"
+import React from "react";
+import { Route } from "react-router-dom";
+import { PostList } from "./post/PostList";
 
 export const ApplicationViews = () => {
   return (
-    <h1 >Welcome to Rare Publishing</h1>
-  )
-}
+    <>
+      <h1>Welcome to Rare Publishing</h1>
+
+      <Route path="/posts">
+        <PostList />
+      </Route>
+    </>
+  );
+};
