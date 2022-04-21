0fbc29547e9305cadbaefefd4fbfc996199434bf
Randall Thomas <randallthomasmusic@gmail.com>
2022-04-21 12:42:40 -0700

    tags list working and had buttons

--- src/components/tags/TagsList.js
+++ src/components/tags/TagsList.js
@@ -1,6 +1,7 @@
 import {useState , useEffect} from "react";
 import { getAllTags } from "./tagsManager";
-
+import {BsFillPencilFill} from "react-icons/bs"
+import "./tags.css"
 export const TagsList = () => {
     const [tags, setTags] = useState([])
 
@@ -16,17 +17,17 @@ export const TagsList = () => {
 
     return (
         <>
-        <div>Tags</div>
+        <h3 className="tags-list">TAGS</h3>
         {
             tags.map(
                 tags => {
                     return <>
-                        <span class="tag is-primary" 
-                            key={`delete--${tags.id}`}>{tags.label}<button 
-                            class="delete"></button>
-                        </span>
-                        <span class="icon has-text-info" key={`edit--${tags.id}`}>
-                        <i class="fas fa-info-circle"></i>
+                        <span class="tag is-primary is-large" 
+                            key={`delete--${tags.id}`}>{tags.label}
+                            {/* delete button */}{/* edit button */}
+                            <div class="edit">{BsFillPencilFill()}</div>
+                            <button class="delete"></button>
+                            
                         </span>
                         </>
                 }
