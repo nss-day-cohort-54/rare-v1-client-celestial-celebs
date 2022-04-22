f74eb8474f1d5f6f5b88318396edf84eb127897c
Airris Williams <airriswilliams83@gmail.com>
2022-04-22 15:37:44 -0500

    longest ticket ever

--- src/components/post/PostList.js
+++ src/components/post/PostList.js
@@ -1,17 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { useHistory, Link } from "react-router-dom";
-import { deletePost, getPosts, deletePost } from "./PostManager";
+import { deletePost, getPosts } from "./PostManager";
 import Post from "./Post";
 import { DeleteDialogue } from "./PostModal";
+import { useModal } from "./UseModal";
 
-export const PostList = ({ onDeleteButtonClick }) => {
+export const PostList = () => {
   const [posts, setPosts] = useState([]);
   const history = useHistory();
-  const { toggleDialogue, modalIsOpen } = useModal("#delete-popup");
+  const [currentPost, setCurrentPost] = useState({});
+
+  const { toggleDialogue, modalIsOpen } = useModal("delete-popup");
 
   useEffect(() => {
     getPosts().then((postData) => setPosts(postData));
-  }, []);
+  }, [currentPost]);
 
   // keyCode is esc key on keyboard, checking if modal is open if popup is open and user presses esc, box closes
   useEffect(() => {
@@ -34,59 +37,54 @@ export const PostList = ({ onDeleteButtonClick }) => {
     deletePost(postId).then(getAllPosts);
   };
 
-  const useModal = (selector) => {
-    const [modalIsOpen, setModalOpen] = useState(false);
-    const toggleDialogue = () => {
-      setModalOpen(!modalIsOpen);
-      if (modalIsOpen) {
-        document.querySelector(`${selector}`).removeAttribute("open");
-      } else {
-        document.querySelector(`${selector}`).removeAttribute("open", true);
-      }
-    };
-    return { toggleDialogue, modalIsOpen };
+  const confirmDelete = (post) => {
+    setCurrentPost(post);
+    toggleDialogue();
   };
 
   return (
-    <div style={{ margin: "0rem 3rem" }}>
-      <h1 className="posts-list">Posts</h1>
+    <>
+      <DeleteDialogue
+        deletePostObject={currentPost}
+        toggleDialogue={toggleDialogue}
+        setCurrentPost={setCurrentPost}
+      />
+      <div style={{ margin: "0rem 3rem" }}>
+        <h1 className="posts-list">Posts</h1>
 
-      {/* <button onClick={() => history.push("/posts/create")}>Add Post</button> */}
+        {/* <button onClick={() => history.push("/posts/create")}>Add Post</button> */}
 
-      <article className="posts">
+        <article className="posts">
+          {posts.map((post) => {
+            return (
+              <section className="post" key={post.id}>
+                <Link to={`/posts/${post.id}`}>
+                  <h3>{post.title}</h3>
+                </Link>
+                <h3>{post.user_id}</h3>
+                <h3>{post.category.label}</h3>
+              </section>
+            );
+          })}
+        </article>
+
+        {/*if the user is logged in on /my-posts, they should be able to delete a post
+          and a message that displays "are you sure you want to delete this post" should appear  */}
         {posts.map((post) => {
           return (
-            <section className="post" key={post.id}>
-              <Link to={`/posts/${post.id}`}>
-                <h3>{post.title}</h3>
-              </Link>
-              <h3>{post.user_id}</h3>
-              <h3>{post.category.label}</h3>
-            </section>
+            <>
+              <div className="panel-block">
+                <Post
+                  key={post.id}
+                  post={post}
+                  user={post.user_id}
+                  confirmDelete={confirmDelete}
+                />
+              </div>
+            </>
           );
         })}
-      </article>
-
-      {/*if the user is logged in on /my-posts, they should be able to delete a post
-          and a message that displays "are you sure you want to delete this post" should appear  */}
-      {posts.map((post) => {
-        return (
-          <>
-            <DeleteDialogue
-              deletePostObject={post}
-              toggleDialogue={toggleDialogue}
-            />
-            <div className="panel-block">
-              <Post
-                key={post.id}
-                post={post}
-                user={users.find((u) => u.id === post.user_id)}
-                onDeleteButtonClick={toggleDialogue()}
-              />
-            </div>
-          </>
-        );
-      })}
-    </div>
+      </div>
+    </>
   );
 };
--- src/components/post/PostModal.js
+++ src/components/post/PostModal.js
@@ -1,13 +1,24 @@
 import { deletePost } from "./PostManager";
+import { useHistory } from "react-router-dom";
 
-export const DeleteDialogue = ({ deletePostObject, toggleDialogue }) => {
+export const DeleteDialogue = ({
+  setCurrentPost,
+  deletePostObject,
+  toggleDialogue,
+}) => {
+  const history = useHistory();
   return (
     <dialog id="delete-popup" className="delete-popup">
       <h2>Are you sure you want to delete this post</h2>
       <div>
         <button
           onClick={() => {
-            deletePost(deletePostObject);
+            deletePost(deletePostObject.id).then(
+              () =>
+                //   history.push("/posts");
+                setCurrentPost({}),
+              toggleDialogue()
+            );
           }}
         >
           Delete Post
