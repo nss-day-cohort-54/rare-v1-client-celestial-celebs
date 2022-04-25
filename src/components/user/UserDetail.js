93fa38aafb7bae5f00c2919cfe6a89ef95df8da9
Airris Williams <airriswilliams83@gmail.com>
2022-04-25 15:41:12 -0500

    updated user details

---
+++ src/components/user/UserDetail.js
@@ -0,0 +1,28 @@
+import { getSingleUser } from "./usersManager";
+import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+
+export const User = () => {
+  const [currentUser, setCurrentUser] = useState();
+  const userId = useParams();
+  // get specific post from API
+  useEffect(() => {
+    getSingleUser(userId).then((data) => setCurrentUser(data));
+  }, []);
+
+  return currentUser ? (
+    <>
+      <h2 className="user_details_full_name">
+        {currentUser.first_name} {currentUser.user.last_name}
+      </h2>
+      <div className="user_details_profile_image">
+        {currentUser.profile_image_url}
+      </div>
+      <div className="user_details_username">{currentUser.username}</div>
+      <div className="user_details_creation_date">{currentUser.created_on}</div>
+      <div className="user_details_bio">{currentUser.bio}</div>
+    </>
+  ) : (
+    ""
+  );
+};
