12c7119a9084efb1ef16c7d91eb61b7fb038e937
Randall Thomas <randallthomasmusic@gmail.com>
2022-04-27 10:59:51 -0700

    flexboxing

--- src/components/comments/CommentList.js
+++ src/components/comments/CommentList.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getPostComments } from "./commentsManager";
-
+import "./CommentsList.css"
 export const CommentsList = () => {
     const [comments, setComments] = useState([])
 
@@ -10,13 +10,13 @@ export const CommentsList = () => {
         getPostComments(postId.postId).then((data) => setComments(data));
       }, []);
     
-      return( 
-                comments.map(comment => {
+      return<div className="comments-container">
+                {comments.map(comment => {
                     return <div class="box" key={comment.id}>
                         <h3>{comment.user.username}</h3>
                         <p>{comment.content}</p>
                     </div>
-                })
-            
-        )
+                })}
+            </div>
+        
 }
